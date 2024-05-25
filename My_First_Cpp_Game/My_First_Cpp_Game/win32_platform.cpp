6c3d9f685511fc8d205047e1f6ea0042f2a4878d
Julia <50312548+julia30oren@users.noreply.github.com>
2024-05-25 10:30:31 -0400

    Next ster - Simulate & render background

--- My_First_Cpp_Game/My_First_Cpp_Game/win32_platform.cpp
+++ My_First_Cpp_Game/My_First_Cpp_Game/win32_platform.cpp
@@ -1,17 +1,49 @@
+#include "utils.cpp"
 #include <windows.h>
 
-bool running = true;
+global_variable bool running = true;
+
+struct Render_State
+{
+    int height, width; 
+    void* memory;
+
+    BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     LRESULT result = 0;
     switch (uMsg) {
-    case WM_CLOSE:
-    case WM_DESTROY: {
-        running = false;
-    } break;
-    default: {
-        result = DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
+        case WM_CLOSE:
+        case WM_DESTROY: {
+            running = false;
+        } break;
+        case WM_SIZE: {
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            render_state.width = rect.right - rect.left;
+            render_state.height = rect.bottom - rect.top;
+
+            int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+            if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+            render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+            // A BITMAPINFOHEADER structure that contains information about the dimensions of color format.
+            render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader); // Specifies the number of bytes required by the structure
+            render_state.bitmap_info.bmiHeader.biWidth = render_state.width;                        // Specifies the width of the bitmap, in pixels
+            render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+            render_state.bitmap_info.bmiHeader.biPlanes = 1;                                  // Specifies the number of planes for the target device
+            render_state.bitmap_info.bmiHeader.biBitCount = 32;                               // Specifies the number of bits per pixel (bpp)
+            render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;                        // For compressed video and YUV formats, this member is a FOURCC code, specified as a DWORD in little-endian order
+        }
+        default: {
+            result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
     }
     return  result;
 }
@@ -29,7 +61,7 @@ int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
     // Register Class ----------------------------------------------------------------------------- 2
     RegisterClass(&window_class);
 
-    // Create Window
+    // Create Window ------------------------------------------------------------------------------ 3
     HWND window = CreateWindowEx(
         0,                                          // Optional window styles.
         window_class.lpszClassName,                 // Window class
@@ -45,6 +77,9 @@ int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
         NULL        // Additional application data
     );
 
+    HDC hdc = GetDC(window);
+
+    // --------------------------------------------------------------------------------------------- 4
     if (window == NULL)
     {
         return 0;
@@ -59,8 +94,14 @@ int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
         }
 
         // Simulate
+        //render_background();
+        clear_screen(0xfff7d5);
+        draw_rect(0, 0, 1, 1, 0xffe370);
+        draw_rect(20, 20, 5, 1, 0xffe370);
+        draw_rect(-40, -10, 2, 6, 0xffe370);
 
         // Render
+        StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 
     }
 }
